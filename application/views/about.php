6cc2cb39cc39a32298809647c9639aab53c31661
Fikri2901 <fikriismudi@gmail.com>
2020-05-05 12:33:30 +0700

    edit about

--- application/views/about.php
+++ application/views/about.php
@@ -1,27 +1,27 @@
 <div class="hero-wrap hero-wrap-2" style="background-image: url('<?php echo base_url(); ?>/asset/images/bg_1.jpg');" data-stellar-background-ratio="0.5">
-      <div class="overlay"></div>
-      <div class="container">
-        <div class="row no-gutters slider-text align-items-end justify-content-start">
-          <div class="col-md-8 ftco-animate text-center text-md-left mb-5">
-          	<p class="breadcrumbs mb-0"><span class="mr-3"><a href="<?php echo base_url(); ?>">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>About</span></p>
-            <h1 class="mb-3 bread">About Us</h1>
-          </div>
-        </div>
-      </div>
-    </div>
+	<div class="overlay"></div>
+	<div class="container">
+		<div class="row no-gutters slider-text align-items-end justify-content-start">
+			<div class="col-md-8 ftco-animate text-center text-md-left mb-5">
+				<p class="breadcrumbs mb-0"><span class="mr-3"><a href="<?php echo base_url(); ?>">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>About</span></p>
+				<h1 class="mb-3 bread">About Us</h1>
+			</div>
+		</div>
+	</div>
+</div>
 
-    <section class="ftco-section">
-    	<div class="container">
-    		<div class="row justify-content-center">
-    			<div class="col-md-10">
-    				<div class="intro row text-center justify-content-center">
-    					<div class="col-md-9">
-    						<img class="img-fluid" src="<?php echo base_url(); ?>/asset/images/undraw_work_time_lhoj.svg" alt="">
-    					</div>
-    					<h2 class="mb-4">Welcome to Jobpply</h2>
-    					<p>JOBPPLY adalah media yang bertujuan menjembatani masyarakat dengan Dunia Usaha dan Dunia Industri (DUDI) dengan mengedepankan kecakapan dan keahlian sesuai yang dibutuhkan pasar kerja. Dalam rangka mempermudah dalam operasional, promosi, mengingat dan komunikasi selanjutnya dipakai nama JOBPPLY.</p>
-    				</div>
-    			</div>
-    		</div>
-    	</div>
-    </section>
+<section class="ftco-section">
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-md-10">
+				<div class="intro row text-center justify-content-center">
+					<div class="col-md-9">
+						<img class="img-fluid" src="<?php echo base_url(); ?>/asset/images/undraw_work_time_lhoj.svg" alt="">
+					</div>
+					<h2 class="mb-4">Welcome to JobPPly</h2>
+					<p>JOBPPLY adalah media yang bertujuan menjembatani masyarakat dengan Dunia Usaha dan Dunia Industri (DUDI) dengan mengedepankan kecakapan dan keahlian sesuai yang dibutuhkan pasar kerja. Dalam rangka mempermudah dalam operasional, promosi, mengingat dan komunikasi selanjutnya dipakai nama JOBPPLY.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
